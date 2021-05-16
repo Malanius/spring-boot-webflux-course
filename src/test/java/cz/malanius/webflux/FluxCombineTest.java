e573ea1eb61f4ab8fd729c2b869687b6b3b0bb70
Malanius Privierre <malaniusprivierre@gmail.com>
2021-05-16 18:56:28 +0200

    Combine streams using concat()

--- src/test/java/cz/malanius/webflux/FluxCombineTest.java
+++ src/test/java/cz/malanius/webflux/FluxCombineTest.java
@@ -34,4 +34,30 @@ class FluxCombineTest {
                 .expectNextCount(6)
                 .verifyComplete();
     }
+
+    @Test
+    void combineUsingConcat() {
+        Flux<String> flux1 = Flux.just("A", "B", "C");
+        Flux<String> flux2 = Flux.just("D", "E", "F");
+
+        Flux<String> merged = Flux.concat(flux1, flux2).log();
+
+        StepVerifier.create(merged)
+                .expectSubscription()
+                .expectNext("A", "B", "C", "D", "E", "F") // will fails as order is not preserved
+                .verifyComplete();
+    }
+
+    @Test
+    void combineUsingConcatWithDelay() {
+        Flux<String> flux1 = Flux.just("A", "B", "C").delayElements(Duration.ofSeconds(1));
+        Flux<String> flux2 = Flux.just("D", "E", "F").delayElements(Duration.ofSeconds(1));
+
+        Flux<String> merged = Flux.concat(flux1, flux2).log();
+
+        StepVerifier.create(merged)
+                .expectSubscription()
+                .expectNext("A", "B", "C", "D", "E", "F") // will not fail as it maintains order, but waits
+                .verifyComplete();
+    }
 }
