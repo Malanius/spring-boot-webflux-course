4cc74ac40ec4cf98fcbee2be9b3a55806174b524
Malanius Privierre <malaniusprivierre@gmail.com>
2021-05-16 16:08:42 +0200

    Add another Flux after error

--- src/test/java/cz/malanius/webflux/FluxAndMonoTest.java
+++ src/test/java/cz/malanius/webflux/FluxAndMonoTest.java
@@ -10,6 +10,7 @@ class FluxAndMonoTest {
         // Creating a flux
         Flux<String> stringFlux = Flux.just("Spring", "Spring Boot", "Reactive Spring")
                 .concatWith(Flux.error(new RuntimeException("Exception occurred.")))
+                .concatWith(Flux.just("After error")) // This won't be emmitet after error is encountered
                 .log();
         // Only on when subscribing flux emits the data
         stringFlux.subscribe(System.out::println, System.err::println); // Second parameter for handling exceptions
