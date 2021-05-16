3807cd6559a3201793b25e53a6b8acfbef7fda9b
Malanius Privierre <malaniusprivierre@gmail.com>
2021-05-16 17:27:39 +0200

    Transform Flux using map()

---
+++ src/test/java/cz/malanius/webflux/FluxMapTest.java
@@ -0,0 +1,37 @@
+package cz.malanius.webflux;
+
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+import java.util.Arrays;
+import java.util.List;
+
+class FluxMapTest {
+
+    List<String> namesList = Arrays.asList("adam", "anna", "jack", "jenny");
+
+    @Test
+    void transformUsingMap() {
+        Flux<String> namesFlux = Flux.fromIterable(namesList) // "adam", "anna", "jack", "jenny"
+                .map(String::toUpperCase) // "ADAM", "ANNA", "JACK", "JENNY"
+                .log();
+
+        StepVerifier.create(namesFlux)
+                .expectNext("ADAM", "ANNA", "JACK", "JENNY")
+                .verifyComplete();
+    }
+
+    @Test
+    void transformUsingMapToLength() {
+        Flux<Integer> namesFlux = Flux.fromIterable(namesList) // "adam", "anna", "jack", "jenny"
+                .map(String::length) // 4, 4, 4, 5
+                .log();
+
+        StepVerifier.create(namesFlux)
+                .expectNext(4, 4, 4, 5)
+                .verifyComplete();
+    }
+
+    
+}
