9bcb7290e12e37fbfffbcbb79ef9adecf08dc0a3
Malanius Privierre <malaniusprivierre@gmail.com>
2021-05-16 20:13:10 +0200

    Hot publisher example

--- src/test/java/cz/malanius/webflux/HotAndColdPublisherTest.java
+++ src/test/java/cz/malanius/webflux/HotAndColdPublisherTest.java
@@ -1,6 +1,7 @@
 package cz.malanius.webflux;
 
 import org.junit.jupiter.api.Test;
+import reactor.core.publisher.ConnectableFlux;
 import reactor.core.publisher.Flux;
 
 import java.time.Duration;
@@ -14,9 +15,23 @@ class HotAndColdPublisherTest {
                 .log();
 
         flux.subscribe(s -> System.out.println("Subscriber 1: " + s)); // emits values from beginning
-        Thread.sleep(2_000);
+        Thread.sleep(3_000);
         flux.subscribe(s -> System.out.println("Subscriber 2: " + s)); // emits values from beginning
         Thread.sleep(4_000);
+    }
 
+    @Test
+    void hotPublisherTest() throws InterruptedException {
+        Flux<String> flux = Flux.just("A", "B", "C", "D", "E", "F")
+                .delayElements(Duration.ofSeconds(1))
+                .log();
+
+        ConnectableFlux<String> connectableFlux = flux.publish();
+        connectableFlux.connect();
+
+        connectableFlux.subscribe(s -> System.out.println("Subscriber 1: " + s)); // doesn't emit values from beginning
+        Thread.sleep(3_000);
+        connectableFlux.subscribe(s -> System.out.println("Subscriber 2: " + s)); // doesn't emit values from beginning
+        Thread.sleep(4_000);
     }
 }
