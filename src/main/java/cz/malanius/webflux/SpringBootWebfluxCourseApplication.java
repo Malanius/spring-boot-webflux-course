0c6a7fea207dbcd44dd050d422a7d8e968931170
Malanius Privierre <malaniusprivierre@gmail.com>
2021-05-16 15:38:20 +0200

    🎉 Initial commit

---
+++ src/main/java/cz/malanius/webflux/SpringBootWebfluxCourseApplication.java
@@ -0,0 +1,13 @@
+package cz.malanius.webflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebfluxCourseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootWebfluxCourseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cz/malanius/webflux/SpringBootWebfluxCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.malanius.webflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebfluxCourseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
