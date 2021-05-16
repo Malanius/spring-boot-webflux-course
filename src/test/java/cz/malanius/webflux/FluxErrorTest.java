2d19c7d82458a12a51de7b811874d9343ba4d974
Malanius Privierre <malaniusprivierre@gmail.com>
2021-05-16 19:11:30 +0200

    Error handling with onErrorResume()

---
+++ src/test/java/cz/malanius/webflux/FluxErrorTest.java
@@ -0,0 +1,28 @@
+package cz.malanius.webflux;
+
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+class FluxErrorTest {
+
+    @Test
+    void errorHandling() {
+        Flux<String> flux = Flux.just("A", "B", "C")
+                .concatWith(Flux.error(new RuntimeException("Exception occurred.")))
+                .concatWith(Flux.just("D"))
+                .onErrorResume(e -> {
+                    System.err.println(e);
+                    return Flux.just("default", "default1");
+                })
+                .log();
+
+        StepVerifier.create(flux)
+                .expectSubscription()
+                .expectNext("A", "B", "C")
+//              .expectError(RuntimeException.class)
+//              .verify();
+                .expectNext("default", "default1")
+                .verifyComplete();
+    }
+}
