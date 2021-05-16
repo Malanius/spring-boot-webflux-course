2a9b761b86e6e62f4a841b0bc71e3723fefe7e8c
Malanius Privierre <malaniusprivierre@gmail.com>
2021-05-16 19:28:56 +0200

    Error handling with retryWhen()

--- src/test/java/cz/malanius/webflux/FluxErrorTest.java
+++ src/test/java/cz/malanius/webflux/FluxErrorTest.java
@@ -1,8 +1,12 @@
 package cz.malanius.webflux;
 
 import org.junit.jupiter.api.Test;
+import reactor.core.Exceptions;
 import reactor.core.publisher.Flux;
 import reactor.test.StepVerifier;
+import reactor.util.retry.Retry;
+
+import java.time.Duration;
 
 class FluxErrorTest {
 
@@ -63,6 +67,29 @@ class FluxErrorTest {
                 .concatWith(Flux.just("D"))
                 .onErrorMap(CustomException::new)
                 .retry(2) // will retry the whole Flux 2 times, totalling in 3 passes
+                .log();
+
+        StepVerifier.create(flux)
+                .expectSubscription()
+                .expectNext("A", "B", "C")
+                .expectNext("A", "B", "C")
+                .expectNext("A", "B", "C")
+                .expectError(CustomException.class)
+                .verify();
+    }
+
+    @Test
+    void errorHandlingWithOnErrorMapWithRetryWhen() {
+        // Defines how to handle retries on errors
+        Retry retrySpec = Retry.fixedDelay(2, Duration.ofSeconds(1)) // retries two times, after 1 s
+                .filter(e -> e instanceof CustomException) // sets which errors are retried
+                .onRetryExhaustedThrow((retryBackoffSpec, retrySignal) -> Exceptions.propagate(retrySignal.failure()));
+
+        Flux<String> flux = Flux.just("A", "B", "C")
+                .concatWith(Flux.error(new RuntimeException("Exception occurred.")))
+                .concatWith(Flux.just("D"))
+                .onErrorMap(CustomException::new)
+                .retryWhen(retrySpec)
                 .log();
 
         StepVerifier.create(flux)
