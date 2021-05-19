83f2c975e8f5ea16cca5d1d4f5f67eeeaf3f053f
Malanius Privierre <malaniusprivierre@gmail.com>
2021-05-19 19:50:33 +0200

    Test the functional endpoints

---
+++ src/test/java/cz/malanius/webflux/router/RouterFunctionConfigTest.java
@@ -0,0 +1,50 @@
+package cz.malanius.webflux.router;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+@AutoConfigureWebTestClient
+class RouterFunctionConfigTest {
+
+    private final WebTestClient testClient;
+
+    @Autowired
+    RouterFunctionConfigTest(WebTestClient testClient) {
+        this.testClient = testClient;
+    }
+
+    @Test
+    void getFlux() {
+        Flux<Integer> flux = testClient.get().uri("/functional/flux")
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .returnResult(Integer.class)
+                .getResponseBody();
+
+        StepVerifier.create(flux)
+                .expectSubscription()
+                .expectNext(1, 2, 3, 4)
+                .verifyComplete();
+    }
+
+    @Test
+    void getMono() {
+        Integer expectedValue = 1;
+        testClient.get().uri("/functional/mono")
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(Integer.class)
+                .consumeWith(response -> assertEquals(expectedValue, response.getResponseBody()));
+    }
+}
