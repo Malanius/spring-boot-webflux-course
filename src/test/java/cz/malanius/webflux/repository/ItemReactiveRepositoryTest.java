da92a1fb8351851b0aac38f95896d5c8087c3505
Malanius Privierre <malaniusprivierre@gmail.com>
2021-05-30 12:15:14 +0200

    Create test for Item repository

---
+++ src/test/java/cz/malanius/webflux/repository/ItemReactiveRepositoryTest.java
@@ -0,0 +1,51 @@
+package cz.malanius.webflux.repository;
+
+import cz.malanius.webflux.document.Item;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.test.context.ActiveProfiles;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+import java.util.Arrays;
+import java.util.List;
+
+@ActiveProfiles("test")
+@DataMongoTest
+class ItemReactiveRepositoryTest {
+
+    private final ItemReactiveRepository repository;
+    private final List<Item> items = Arrays.asList(
+            new Item(null, "Something", 500.5),
+            new Item(null, "Anything", 20.0),
+            new Item(null, "Everything", 999.9)
+    );
+
+    @Autowired
+    ItemReactiveRepositoryTest(ItemReactiveRepository repository) {
+        this.repository = repository;
+    }
+
+    @BeforeEach
+    void setUp() {
+        repository.insert(Flux.fromIterable(items))
+                .doOnNext(item -> System.out.println("Inserted item: " + item))
+                .blockLast(); // Waits for completion of all preceding call
+    }
+
+    @AfterEach
+    void tearDown() {
+        repository.deleteAll();
+    }
+
+    @Test
+    void getAllItems() {
+        StepVerifier.create(repository.findAll())
+                .expectSubscription()
+                .expectNextCount(items.size())
+                .verifyComplete();
+    }
+}
