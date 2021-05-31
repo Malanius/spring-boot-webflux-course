59bd832b2636958fcef775240857e032509e8f6b
Malanius Privierre <malaniusprivierre@gmail.com>
2021-05-31 17:38:02 +0200

    Create endpoint to get single item

--- src/main/java/cz/malanius/webflux/controller/ItemController.java
+++ src/main/java/cz/malanius/webflux/controller/ItemController.java
@@ -4,10 +4,13 @@ import cz.malanius.webflux.document.Item;
 import cz.malanius.webflux.repository.ItemReactiveRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @Slf4j
 @RestController
@@ -24,5 +27,12 @@ public class ItemController {
     @GetMapping()
     public Flux<Item> getAllItems() {
         return repository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Mono<ResponseEntity<Item>> getItem(@PathVariable String id) {
+        return repository.findById(id)
+                .map(ResponseEntity::ok)
+                .defaultIfEmpty(ResponseEntity.notFound().build());
     }
 }
