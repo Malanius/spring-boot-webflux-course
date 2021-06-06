116eb9d2eda8017362b1b14191e1bc2a4e86cb56
Malanius Privierre <malaniusprivierre@gmail.com>
2021-06-06 16:39:59 +0200

    Create repository with tailable cursor

---
+++ items-server/src/main/java/cz/malanius/webflux/repository/ItemReactiveCappedRepository.java
@@ -0,0 +1,13 @@
+package cz.malanius.webflux.repository;
+
+import cz.malanius.webflux.document.ItemCapped;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.data.mongodb.repository.Tailable;
+import reactor.core.publisher.Flux;
+
+public interface ItemReactiveCappedRepository extends ReactiveMongoRepository<ItemCapped, String> {
+
+    @Tailable
+    Flux<ItemCapped> findItemsBy();
+    
+}
